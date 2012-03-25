3256eb4f5d06b6fff3984356f13968d9ebb8d054
asndav <asndav@gmail.com>
2012-03-25 22:11:57 +0200

    EditText

--- src/es/perdidos/android/MarcadorActivity.java
+++ src/es/perdidos/android/MarcadorActivity.java
@@ -2,12 +2,24 @@ package es.perdidos.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MarcadorActivity extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button calcularButton = (Button) findViewById(R.id.calcular);
+        calcularButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MarcadorActivity.this, "Calcular", Toast.LENGTH_SHORT).show();
+			}
+		});
     }
 }
